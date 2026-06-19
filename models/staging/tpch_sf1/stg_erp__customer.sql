9951bc22860779609682db90031232314ba83644
Jules Bernard <jbernard@synergy.fr>
2026-06-19 13:15:18 +0000

    ajout des sources

---
+++ models/staging/tpch_sf1/stg_erp__customer.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized='table',
+        database='TRAV_DWH_DEV',
+        schema='dbo'
+    )
+}}
+with 
+
+source as (
+
+    select * from {{ source('tpch_sf1', 'customer') }}
+
+),
+
+renamed as (
+
+    select
+        c_custkey,
+        c_name,
+        c_address,
+        c_nationkey,
+        c_phone,
+        c_acctbal,
+        c_mktsegment,
+        c_comment
+
+    from source
+
+)
+
+select * from renamed
